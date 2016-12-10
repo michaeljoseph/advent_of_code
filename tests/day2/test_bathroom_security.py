c38833ea41d9340eb1c770ace8cb4d6b6c48ce4b
Michael Joseph <m.joseph@quidco.com>
2016-12-10 18:03:10 +0200

    First failing test

--- tests/day2/test_bathroom_security.py
+++ tests/day2/test_bathroom_security.py
@@ -1,2 +1,12 @@
-def test_it():
-    assert False
+from day2.bathroom_security import decode
+
+
+def test_decode():
+    instructions = [
+        'ULL'
+        'RRDDD'
+        'LURDL'
+        'UUUUD'
+    ]
+
+    assert decode(instructions) == '1985'
