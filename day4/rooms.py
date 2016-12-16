f7c7b2a9e58d4b88b3f962250264776b11f09115
Michael Joseph <m.joseph@quidco.com>
2016-12-16 18:23:41 +0200

    Refactor io boundaries for silver star
    Also new function and test

--- day4/rooms.py
+++ day4/rooms.py
@@ -4,13 +4,17 @@ from collections import defaultdict, Counter
 
 def parse_room(room):
     room_parts = room.split('-')
-    encrypted_name = ''.join(room_parts[:-1])
+    encrypted_name = '-'.join(room_parts[:-1])
     sector_id, checksum = re.match(r'(\d*)\[(.*)\].*', room_parts[-1:][0]).groups()
-    return encrypted_name, sector_id, checksum
+    return encrypted_name, int(sector_id), checksum
+
+
+def decrypt_room_name(room, sector_id):
+    return 'very encrypted name'
 
 
 def calculate_checksum(encrypted_name):
-    c = Counter(encrypted_name)
+    c = Counter(encrypted_name.replace('-', ''))
     occurrence_map = defaultdict(lambda: [])
 
     for value, occurrences in c.most_common():
@@ -29,7 +33,7 @@ def valid_rooms_sector_sum(rooms):
     for room in rooms:
         encrypted_name, sector_id, checksum = parse_room(room)
         if checksum == calculate_checksum(encrypted_name):
-            sector_id_total += int(sector_id)
+            sector_id_total += sector_id
 
     return sector_id_total
 
--- tests/day4/test_rooms.py
+++ tests/day4/test_rooms.py
@@ -1,27 +1,27 @@
 import pytest
-from day4.rooms import parse_room, calculate_checksum, valid_rooms_sector_sum
+from day4.rooms import parse_room, calculate_checksum, valid_rooms_sector_sum, decrypt_room_name
 
 
 def test_parse_room():
     room = 'aaaaa-bbb-z-y-x-123[abxyz]'
     encrypted_name, sector_id, checksum = parse_room(room)
 
-    assert encrypted_name == 'aaaaabbbzyx'
-    assert sector_id == '123'
+    assert encrypted_name == 'aaaaa-bbb-z-y-x'
+    assert sector_id == 123
     assert checksum == 'abxyz'
 
 
 @pytest.mark.parametrize('encrypted_name, checksum', [
-    ('aaaaabbbzyx', 'abxyz'),
-    ('abcdefgh', 'abcde'),
-    ('notarealroom', 'oarel'),
+    ('aaaaa-bbb-z-y-x', 'abxyz'),
+    ('a-b-c-d-e-f-g-h', 'abcde'),
+    ('not-a-real-room', 'oarel'),
 ])
 def test_checksum(encrypted_name, checksum):
     assert calculate_checksum(encrypted_name) == checksum
 
 
 @pytest.mark.parametrize('encrypted_name, checksum', [
-    ('totallyrealroom', 'decoy'),
+    ('totally-real-room', 'decoy'),
 ])
 def test_bad_checksum(encrypted_name, checksum):
     assert not calculate_checksum(encrypted_name) == checksum
@@ -35,3 +35,9 @@ def test_sector_sum():
         'totally-real-room-200[decoy]',
     ]
     assert 1514 == valid_rooms_sector_sum(rooms)
+
+
+def test_decrypt_room_name():
+    encrypted_name = 'qzmt-zixmtkozy-ivhz-343'
+    sector_id = 343
+    assert decrypt_room_name(encrypted_name, sector_id) == 'very encrypted name'
