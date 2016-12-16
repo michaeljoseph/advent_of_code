a93a90daefdde03bdd6bd9bef6136b37a655b9e7
Michael Joseph <m.joseph@quidco.com>
2016-12-17 00:43:36 +0200

    Silver star

--- day5/password.py
+++ day5/password.py
@@ -2,6 +2,10 @@ import md5
 import sys
 
 
+def sequential_digit(hash):
+    return hash[5]
+
+
 def generate_password_digits(door_id, limit=8):
     found = 0
     x = 1
@@ -10,11 +14,47 @@ def generate_password_digits(door_id, limit=8):
         hash = str(md5.new(current).hexdigest())
 
         if hash.startswith('00000'):
-            found += 1
-            yield hash[5]
+            digit = sequential_digit(hash)
+            if digit is not None:
+                found += 1
+                yield digit
         x += 1
 
 
-door_id = sys.argv[1]
+def find_password_digits(door_id, password_length=8):
+    password = [None] * password_length
 
-print(''.join([password_digit for password_digit in generate_password_digits(door_id)]))
+    password_found = False
+    x = 1
+    while not password_found:
+        current = door_id + str(x)
+
+        hash = str(md5.new(current).hexdigest())
+        if hash.startswith('00000'):
+            try:
+                position = int(hash[5])
+                value = hash[6]
+
+                if password[position] is None:
+                    password[position] = value
+
+            except (ValueError, IndexError):
+                x += 1
+                continue
+
+            print(hash, position, value, password)
+        x += 1
+        password_found = all([digit is not None for digit in password])
+
+    return ''.join(password)
+
+
+def door_id_password(door_id):
+    return ''.join([
+        password_digit for password_digit in generate_password_digits(door_id)
+    ])
+
+if __name__ == '__main__':
+    door_id = sys.argv[1]
+    # print(door_id_password(door_id))
+    print(find_password_digits(door_id))
