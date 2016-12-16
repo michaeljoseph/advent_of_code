b2e71c4f08c26d0d4851a52299a042cdeb29e147
Michael Joseph <m.joseph@quidco.com>
2016-12-16 18:14:47 +0200

    Input file harness for gold star answer

--- day4/rooms.py
+++ day4/rooms.py
@@ -5,7 +5,7 @@ from collections import defaultdict, Counter
 def parse_room(room):
     room_parts = room.split('-')
     encrypted_name = ''.join(room_parts[:-1])
-    sector_id, checksum = re.match(r'(\d*)\[(.*)\]', room_parts[-1:][0]).groups()
+    sector_id, checksum = re.match(r'(\d*)\[(.*)\].*', room_parts[-1:][0]).groups()
     return encrypted_name, sector_id, checksum
 
 
@@ -32,3 +32,12 @@ def valid_rooms_sector_sum(rooms):
             sector_id_total += int(sector_id)
 
     return sector_id_total
+
+
+if __name__ == '__main__':
+    import sys, io
+
+    input_file = sys.argv[1]
+
+    rooms = io.open(input_file).readlines()
+    print(valid_rooms_sector_sum(rooms))
