5a0616709954e4d19b6b193fe9e4ae08b38eff78
Michael Joseph <m.joseph@quidco.com>
2016-12-21 01:13:05 +0200

    Second star, row/column rotation

--- day8/twofa.py
+++ day8/twofa.py
@@ -1,32 +1,113 @@
-
-# The screen is 50 pixels wide and 6 pixels tall, all of which start off
-
-# rect AxB
-# rotate row y=A by B
-# rotate column x=A by B
 import re
+from copy import deepcopy
 
 RECT = 'rect'
+COLUMN = 'rotate column'
+ROW = 'rotate row'
+SCREEN = (50, 6)
 
 
 def parse_dimensions(dimensions):
     return re.match(r'(\d*)x(\d*)', dimensions).groups()
 
 
+def parse_rotation(rotation):
+    return re.match(r'.*=(\d*) by (\d*)', rotation).groups()
+
+
 def parse_instruction(instruction):
-    if instruction.startswith('rect'):
+    if instruction.startswith(RECT):
         x, y = parse_dimensions(instruction[5:])
         return RECT, int(x), int(y)
+
+    elif instruction.startswith(COLUMN):
+        column, increment = parse_rotation(instruction[len(COLUMN)+1:])
+        return COLUMN, int(column), int(increment)
+    elif instruction.startswith(ROW):
+        row, increment = parse_rotation(instruction[len(ROW)+1:])
+        return ROW, int(row), int(increment)
 
     return False
 
 
-def parse_instruction_for_pixels(instruction):
+def initialise_screen(columns, rows):
+    screen = []
 
+    for y in range(rows):
+        screen.append(['.'] * columns)
+
+    return screen
+
+
+def parse_screen(data):
+    screen = []
+    for line in data:
+        screen.append([x for x in line])
+    return screen
+
+
+def rotate_column(data, column, increment):
+    tmp_data = deepcopy(data)
+
+    for row_idx, row in enumerate(data):
+        target_row_index = row_idx - increment
+        if target_row_index < 0:
+            target_row_index += len(data)
+
+        data[row_idx][column] = tmp_data[target_row_index][column]
+
+    return data
+
+
+def rotate_row(data, row, increment):
+    tmp_data = deepcopy(data)
+
+    for col_idx, col in enumerate(data[row]):
+        new_idx = col_idx - increment
+        if new_idx < 0:
+            new_idx += len(data[row])
+        data[row][col_idx] = tmp_data[row][new_idx]
+
+    return data
+
+
+def apply_instruction(screen, instruction):
+    op, x, y = parse_instruction(instruction)
+
+    if op == RECT:
+        for i in range(x):
+            for j in range(y):
+                screen[j][i] = '#'
+
+    elif op == COLUMN:
+        column, increment = x, y
+        screen = rotate_column(screen, column, increment)
+
+    elif op == ROW:
+        row, increment = x, y
+        screen = rotate_row(screen, row, increment)
+
+    return screen
+
+
+def apply_instructions(screen, instructions):
+    for instruction in instructions:
+        screen = apply_instruction(screen, instruction)
+
+    return screen
+
+
+def print_screen(screen):
+    for line in screen:
+        print(''.join(line))
+
+
+def parse_instruction_for_pixels(instruction):
     result = parse_instruction(instruction)
     if result:
         op, x, y = result
-        return x * y
+        if op == RECT:
+            return x * y
     return 0
 
 
@@ -44,4 +125,11 @@ if __name__ == '__main__':
     input_file = sys.argv[1]
     instructions = [x.strip() for x in io.open(input_file).readlines()]
 
-    print(how_many_pixels_are_lit(instructions))
+    print('{} pixels are lit'.format(how_many_pixels_are_lit(instructions)))
+
+    screen = initialise_screen(SCREEN[0], SCREEN[1])
+
+    screen = apply_instructions(screen, instructions)
+
+    # ZJHRKCPLYJ
+    print_screen(screen)
--- tests/day8/test_2fa.py
+++ tests/day8/test_2fa.py
@@ -7,8 +7,152 @@ def test_parse_dimensions():
     assert parse_dimensions('342x42') == ('342', '42')
 
 
-def test_parse_instruction():
+def test_parse_instruction_rect():
     assert parse_instruction('rect 3x2') == (RECT, 3, 2)
+
+
+def test_parse_rotation():
+    assert parse_rotation('x=1 by 1') == ('1', '1')
+
+
+def test_parse_instruction_row():
+    assert parse_instruction('rotate column x=1 by 1') == (COLUMN, 1, 1)
+
+
+def test_initialise_screen():
+    assert (
+        initialise_screen(2, 2) == [
+            ['.', '.'],
+            ['.', '.'],
+        ]
+    )
+
+
+def test_apply_rect_instruction():
+    screen = initialise_screen(2, 2)
+
+    instruction = 'rect 1x1'
+    expected_screen = [
+        ['#', '.'],
+        ['.', '.'],
+    ]
+    assert apply_instruction(screen, instruction) == expected_screen
+
+
+def test_apply_rect_large():
+    screen = initialise_screen(7, 3)
+    instruction = 'rect 3x2'
+    expected_screen = [
+        ['#', '#', '#', '.', '.', '.', '.'],
+        ['#', '#', '#', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+    ]
+    assert apply_instruction(screen, instruction) == expected_screen
+
+
+def test_parse_screen():
+    screen = [
+        '#.',
+        '#.',
+        '..',
+    ]
+    expected_screen = [
+        ['#', '.'],
+        ['#', '.'],
+        ['.', '.'],
+    ]
+    assert parse_screen(screen) == expected_screen
+
+
+def test_rotate_column():
+    screen = [
+        ['#', '.'],
+        ['#', '.'],
+        ['.', '.'],
+    ]
+    expected_screen = [
+        ['.', '.'],
+        ['#', '.'],
+        ['#', '.'],
+    ]
+    assert rotate_column(screen, 0, 1) == expected_screen
+
+
+def test_rotate_row():
+    screen = parse_screen([
+        '#.#....',
+        '###....',
+        '.#.....',
+    ])
+
+    expected_screen = parse_screen([
+        '....#.#',
+        '###....',
+        '.#.....',
+    ])
+
+    assert rotate_row(screen, 0, 4) == expected_screen
+
+
+def test_rotate_row_input():
+    screen = parse_screen([
+        '#.................................................'
+    ])
+    expected_screen = parse_screen([
+        '..#...............................................'
+    ])
+    assert rotate_row(screen, 0, 2) == expected_screen
+
+
+def test_apply_rotate_column():
+    screen = parse_screen([
+        '###....',
+        '###....',
+        '.......',
+    ])
+    instruction = 'rotate column x=1 by 1'
+
+    expected_screen = parse_screen([
+        '#.#....',
+        '###....',
+        '.#.....',
+    ])
+
+    assert apply_instruction(screen, instruction) == expected_screen
+
+
+def test_apply_rotate_row():
+    screen = parse_screen([
+        '#.#....',
+        '###....',
+        '.#.....',
+    ])
+    instruction = 'rotate row y=0 by 4'
+    expected_screen = parse_screen([
+        '....#.#',
+        '###....',
+        '.#.....',
+    ])
+
+    assert apply_instruction(screen, instruction) == expected_screen
+
+
+def test_apply_instructions():
+    screen = initialise_screen(7, 3)
+
+    expected_screen = parse_screen([
+        '.#..#.#',
+        '#.#....',
+        '.#.....',
+    ])
+
+    instructions = [
+        'rect 3x2',
+        'rotate column x=1 by 1',
+        'rotate row y=0 by 4',
+        'rotate column x=1 by 1',
+    ]
+    assert apply_instructions(screen, instructions) == expected_screen
 
 
 def test_parse_instruction_for_pixels():
