7176e912b54e84da26d7fb734d599140743ee059
Michael Joseph <michaeljoseph+github@gmail.com>
2018-12-05 15:13:57 +0200

    2016 wip changes

--- day9/explosives.py
+++ day9/explosives.py
@@ -16,7 +16,8 @@ def extract_marker(data, current):
     return marker, marker_end
 
 
-def decompress(data):
+def decompress(data, markers=False):
+    decompressed_length = 0
     decompressed_data = ''
 
     data = data.replace(' ', '')
@@ -27,16 +28,19 @@ def decompress(data):
     if data.find(MARKER_START) == -1:
         done = True
         decompressed_data = data
+        decompressed_length = len(data)
 
     while not done:
         marker_start = data.find(MARKER_START, current)
         # no more markers, copy to end of data
         if marker_start == -1:
             done = True
+            decompressed_length += len(data[current:])
             decompressed_data += data[current:]
             break
         else:
             # copy from current to marker_start
+            decompressed_length += marker_start - current
             decompressed_data += data[current: marker_start]
 
         marker_end = data.find(')', marker_start)
@@ -45,10 +49,50 @@ def decompress(data):
 
         current = marker_end + 1 + num_chars
         repeat_data = data[marker_end + 1: current]
-        decompressed_data += repeat_data * num_repeats
 
+        if markers:
+            repeat_data = decompress(repeat_data, markers=True)
+
+        decompressed_data += repeat_data * num_repeats
+        decompressed_length += len(repeat_data) * num_repeats
+
+    # assert len(decompressed_data) == decompressed_length
+    # print(len(decompressed_data), decompressed_length)
     return decompressed_data
 
+
+def decompress_length(data):
+    decompressed_length = 0
+    data = data.replace(' ', '')
+
+    done = False
+    current = 0
+
+    if data.find(MARKER_START) == -1:
+        done = True
+        decompressed_length = len(data)
+
+    while not done:
+        marker_start = data.find(MARKER_START, current)
+        # no more markers, copy to end of data
+        if marker_start == -1:
+            done = True
+            decompressed_length += len(data[current:])
+            break
+        else:
+            # copy from current to marker_start
+            decompressed_length += marker_start - current
+
+        marker_end = data.find(')', marker_start)
+        marker = data[marker_start + 1: marker_end]
+        num_chars, num_repeats = parse_marker(marker)
+
+        current = marker_end + 1 + num_chars
+        repeat_data = data[marker_end + 1: current]
+
+        decompressed_length += decompress_length(repeat_data) * num_repeats
+
+    return decompressed_length
 
 if __name__ == '__main__':
     import sys, io
@@ -56,6 +100,7 @@ if __name__ == '__main__':
     input_file = sys.argv[1]
     compressed = io.open(input_file).read().strip()
 
-    decompressed = decompress(compressed)
-    print(decompressed)
-    print(len(decompressed))
+    # decompressed = decompress(compressed, markers=True)
+    # print(len(decompressed))
+    decompressed_length= decompress(compressed, markers=True)
+    print(decompressed_length)
--- tests/day9/test_explosives.py
+++ tests/day9/test_explosives.py
@@ -22,7 +22,29 @@ def test_parse_marker():
     ('(6x1)(1x3)A', '(1x3)A', 6),
     ('X(8x2)(3x3)ABCY', 'X(3x3)ABC(3x3)ABCY', 18),
 ])
-def test_single_marker(compressed, decompressed, length):
+def test_decompress(compressed, decompressed, length):
     decompressed_data = decompress(compressed)
     assert decompressed_data == decompressed
     assert len(decompressed_data) == length
+
+
+@pytest.mark.parametrize('compressed, length', [
+    ('(3x3)XYZ', 9),
+    ('X(8x2)(3x3)ABCY', len('XABCABCABCABCABCABCY')),
+    ('(27x12)(20x12)(13x14)(7x10)(1x12)A', 241920),
+    ('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN', 445),
+])
+def test_v2_decompress_length(compressed, length):
+    decompressed_data = decompress(compressed, markers=True)
+    assert len(decompressed_data) == length
+
+
+@pytest.mark.parametrize('compressed, length', [
+    ('(3x3)XYZ', 9),
+    ('X(8x2)(3x3)ABCY', len('XABCABCABCABCABCABCY')),
+    ('(27x12)(20x12)(13x14)(7x10)(1x12)A', 241920),
+    ('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN', 445),
+])
+def test_decompress_length(compressed, length):
+    decompressed_len = decompress_length(compressed)
+    assert decompressed_len == length
